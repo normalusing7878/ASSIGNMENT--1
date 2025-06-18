2c8bc047d9d1f5dd1fbb3e1f0fb1546bf51f32c8
normalusing7878 <mohammadslahuddin39@gmail.com>
2025-06-18 14:36:48 +0530

    Add files via upload

---
+++ ASSIGNMENT 1.py
@@ -0,0 +1,23 @@
+#task 1
+n1=input('Enter the first number: ')
+n2=input('Enter the second number: ')
+
+n1=int(n1)
+n2=int(n2)
+
+add=n1+n2
+subtraction=n1-n2
+multiplication=n1*n2
+division=n1/n2
+print('addition: ',add)
+print('subtraction: ',subtraction)
+print('multiplication: ',multiplication)
+print('division:',division)
+
+#task 2
+first_name=input('Enter your first name: ')
+last_name=input('Enter your second name: ')
+
+full_name= first_name + " "+last_name
+
+print(f"hellow, {full_name} welcome to python program")
